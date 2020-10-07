ea70af08109ef6fe26182fd202576d4d286d8719
Mariano Burguete <corsaiir@gmail.com>
2020-10-07 18:40:22 -0300

    pronta la araña workana

--- backend/projectScrapy/projectScrapy/spiders/workana.py
+++ backend/projectScrapy/projectScrapy/spiders/workana.py
@@ -1,25 +1,26 @@
-from scrapy.exceptions import CloseSpider
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-from scrapy.exceptions import CloseSpider
-from projectScrapy.items import WOItem
 import scrapy
+from projectScrapy.items import WOItem
+from scrapy.exceptions import CloseSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-
+def extract(raw_string, start_marker, end_marker):
+    start = raw_string.index(start_marker) + len(start_marker)
+    end = raw_string.index(end_marker, start)
+    return raw_string[start:end]
 
 class WorkanaSpider(CrawlSpider):
     name = 'workana'
     allowed_domains = ['www.workana.com']
-    start_urls = ['https://www.workana.com/jobs?country=UY']
-
-    items_count = 0
-    page_count = 2
+    start_urls = [
+    'https://www.workana.com/jobs?country=UY','https://www.workana.com/jobs?country=UY&page=2','https://www.workana.com/jobs?country=UY&page=3','https://www.workana.com/jobs?country=UY&page=4','https://www.workana.com/jobs?country=UY&page=5','https://www.workana.com/jobs?country=UY&page=6','https://www.workana.com/jobs?country=UY&page=7','https://www.workana.com/jobs?country=UY&page=8','https://www.workana.com/jobs?country=UY&page=9','https://www.workana.com/jobs?country=UY&page=10',
+    'https://www.workana.com/jobs?country=UY&page=11','https://www.workana.com/jobs?country=UY&page=12','https://www.workana.com/jobs?country=UY&page=13','https://www.workana.com/jobs?country=UY&page=14','https://www.workana.com/jobs?country=UY&page=15','https://www.workana.com/jobs?country=UY&page=16','https://www.workana.com/jobs?country=UY&page=17','https://www.workana.com/jobs?country=UY&page=18','https://www.workana.com/jobs?country=UY&page=19','https://www.workana.com/jobs?country=UY&page=20',
+    'https://www.workana.com/jobs?country=UY&page=21','https://www.workana.com/jobs?country=UY&page=22','https://www.workana.com/jobs?country=UY&page=23','https://www.workana.com/jobs?country=UY&page=24','https://www.workana.com/jobs?country=UY&page=25','https://www.workana.com/jobs?country=UY&page=26','https://www.workana.com/jobs?country=UY&page=27','https://www.workana.com/jobs?country=UY&page=28','https://www.workana.com/jobs?country=UY&page=29','https://www.workana.com/jobs?country=UY&page=30',
+    'https://www.workana.com/jobs?country=UY&page=31','https://www.workana.com/jobs?country=UY&page=32','https://www.workana.com/jobs?country=UY&page=33','https://www.workana.com/jobs?country=UY&page=34','https://www.workana.com/jobs?country=UY&page=35','https://www.workana.com/jobs?country=UY&page=36','https://www.workana.com/jobs?country=UY&page=37','https://www.workana.com/jobs?country=UY&page=38','https://www.workana.com/jobs?country=UY&page=39','https://www.workana.com/jobs?country=UY&page=40',
+    'https://www.workana.com/jobs?country=UY&page=41','https://www.workana.com/jobs?country=UY&page=42','https://www.workana.com/jobs?country=UY&page=43','https://www.workana.com/jobs?country=UY&page=44','https://www.workana.com/jobs?country=UY&page=45','https://www.workana.com/jobs?country=UY&page=46','https://www.workana.com/jobs?country=UY&page=47','https://www.workana.com/jobs?country=UY&page=48','https://www.workana.com/jobs?country=UY&page=49','https://www.workana.com/jobs?country=UY&page=50',
+    ]
 
     rules = (
-        Rule(LinkExtractor(
-            allow=(), restrict_xpaths='//ul[@class="pagination"]//li//a[contains(text(),' + f'{page_count}' + ')]'),
-            callback='func',
-            follow=True),
         Rule(LinkExtractor(
             allow=(), restrict_xpaths='//*[@id="projects"]/div/div[1]/h2/a'),
             callback='parse_item',
@@ -27,35 +28,17 @@ class WorkanaSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        # if self.items_count > 1000:
-        #     raise CloseSpider('items exceeded')
-        # self.items_count += 1
-
-
+        #extract(fields, 'Categoría <strong>', '</strong>') EJEMPLO PARA EXTRAER TAGS DEL RESUME
+        fields = response.xpath('//p[@class="resume"]').get()
         it = WOItem()
         it['url'] = response.request.url
         it['title'] = response.xpath('normalize-space(//*[@id="productName"]/h1/text())').get()
-        it['workday'] = response.xpath(
-            'normalize-space(//*[@id="app"]/div/div/section/section/div/section/article/p/strong[5]//text())').get()
-        it['contract_type'] = response.xpath(
-            'normalize-space(//*[@id="app"]/div/div[2]/section/section[1]/div/section/article[1]/p[5]/text())').get()
+        it['workday'] = extract(fields, 'Disponibilidad requerida <strong>', '</strong>')
+        it['roles'] = extract(fields, 'Roles necesarios <strong>', '</strong>')
         it['salary'] = response.xpath(
             'normalize-space(//*[@id="app"]/div/div[2]/section/section[1]/div/section/article[1]/div[1]/div[2]/h4/text())').get()
         it['description'] = response.xpath(
             'normalize-space(//*[@id="app"]/div/div[2]/section/section[1]/div/section/article[1]/div[2]/text())').get()
-        it['requirements'] = response.xpath('normalize-space(//p[@class="skills"]//text())').get()
-        it['company_name'] = response.xpath('//*[@id="app"]/div/div[2]/section/section[1]/div/aside/article[3]/div[1]/a/span/text()').get()
-        it['prueba'] = response.xpath('//p[@class="resume"]').get()
-
-        
-        #next_url = response.xpath('//ul[@class="pagination"]//li//a[contains(text(),' + self.page_count + ')]/@href') #response.css('#pagination > ul > li > a[href~="?page={0}"]::attr(href)'.format(self.page_count))
-        
+        it['requirements'] = " ".join(response.xpath('normalize-space(//p[@class="skills"])').getall())
+        it['company_name'] = response.xpath('normalize-space(//*[@id="app"]/div/div[2]/section/section[1]/div/aside/article[3]/div[1]/a/span/text())').get()
         yield it
-        # if next_url:
-        #     yield scrapy.Request(
-        #         url = next_url
-        #     )
-        
-    def func(self, response):
-        self.page_count += 1
-        print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
