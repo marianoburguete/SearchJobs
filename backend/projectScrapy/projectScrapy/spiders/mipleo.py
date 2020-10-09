cea7a3ce23787d0b37a63dcd48e5066c335edaff
MaximilianoLan <38016059+MaximilianoLan@users.noreply.github.com>
2020-10-09 19:22:15 -0300

    Changes to spyder

--- backend/projectScrapy/projectScrapy/spiders/mipleo.py
+++ backend/projectScrapy/projectScrapy/spiders/mipleo.py
@@ -22,32 +22,35 @@ class MipleoSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        # if self.items_count > 20:
-        #     raise CloseSpider('items exceeded')
-        # self.items_count += 1
+        if self.items_count > 20:
+            raise CloseSpider('items exceeded')
+        self.items_count += 1
         it = MTItem()
         it['url'] = response.request.url
         it['title'] = response.xpath('normalize-space(//h1/text())').get()
         it['location'] = response.xpath(
-            '//*[@id="content"]/div[3]/div[2]/ul/li[4]/b/text()').get()
+            'normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[4]/b/text())').get()
         it['workday'] = response.xpath(
-            '//*[@id="content"]/div[3]/div[2]/ul/li[6]/b/text()').get()
+            'normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[6]/b/text())').get()
         it['contract_type'] = response.xpath(
-            '//*[@id="content"]/div[3]/div[2]/ul/li[7]/b/text()').get()
+            'normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[7]/b/text())').get()
         it['salary'] = response.xpath(
-            '//*[@id="content"]/div[3]/div[2]/ul/li[1]/b/text()').get()
+            'normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[1]/b/text())').get()
         it['description'] = response.xpath(
-            '//*[@id="content"]/div[3]/div[2]/div[2]/p/text()').get()
+            'normalize-space(//*[@id="content"]/div[3]/div[2]/div[2]/p/text())').get()
         requirements = []
-        for ul in response.xpath('//*[@id="content"]/div[3]/div[2]/ul/li').getall():
-            print("PRUEBA: " + str(type(ul)) )
-            prueba = ul.xpath("text()").getall()
-            for index, x in prueba:
-                requirements.append(x)
-            
+        for ul in response.xpath('//*[@id="content"]/div[3]/div[2]/ul//li//text()').getall():
+            if 'Idiomas:' in ul:
+                requirements.append(response.xpath('normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[contains(text(), "Idiomas")])').get())
+        for ul in response.xpath('//*[@id="content"]/div[3]/div[2]/ul//li//text()').getall():
+            if 'Disponibilidad de cambio de residencia:' in ul:
+                requirements.append(response.xpath('normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[contains(text(), "Disponibilidad de cambio de residencia:")])').get())
+        for ul in response.xpath('//*[@id="content"]/div[3]/div[2]/ul//li//text()').getall():
+            if 'Disponibilidad de viajar:' in ul:
+                requirements.append(response.xpath('normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[contains(text(), "Disponibilidad de viajar:")])').get())
+        for ul in response.xpath('//*[@id="content"]/div[3]/div[2]/ul//li//text()').getall():
+            if 'Educación Mínima:' in ul:
+                requirements.append(response.xpath('normalize-space(//*[@id="content"]/div[3]/div[2]/ul/li[contains(text(), "Educación Mínima:")])').get())           
         it['requirements'] = requirements.copy()
         it['company_name'] = response.xpath('//*[@id="content"]/div[4]/div[1]/span/text()').get()
         yield it
-
-        #for ul in response.xpath('//*[@id="content"]/div[3]/div[2]/ul/text()').getall():
-        #for ul in response.xpath('//ul[@class="info_item"]//li/text()').getall():
