cbf1418ecd3b5eee33e51621380b1fa4175404f0
Lucas Montelongo <37620501+lucasmontelongo@users.noreply.github.com>
2020-10-24 16:51:07 -0300

    nuevas tablas

--- backend/api/app/database/CurriculumModel.py
+++ backend/api/app/database/CurriculumModel.py
@@ -4,8 +4,16 @@ class Curriculum(db.Model, BaseModelMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     birth_date = db.Column(db.Integer) #esto tiene que se un datetime, lo dejo asi para probar
+    phone = db.Column(db.Integer)
+    country = db.Column(db.String)
+    address = db.Column(db.String)
+    avatar = db.Column(db.String)
+    description = db.Column(db.String)
+    education = db.relationship('Education', backref='curriculum', lazy=False, cascade='all, delete-orphan')
+    workexperience = db.relationship('WorkExperience', backref='curriculum', lazy=False, cascade='all, delete-orphan')
+    languages = db.relationship('Language', backref='curriculum', lazy=False, cascade='all, delete-orphan')
+    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, name, birth_date):
+    def __init__(self, name):
         self.name = name
-        self.birth_date = birth_date
---
+++ backend/api/app/database/LanguageModel.py
@@ -0,0 +1,8 @@
+from app.database.db import db, BaseModelMixin
+
+class Language(db.Model, BaseModelMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
