4f434bc81401da59f8f8290b4d8751375e9cddf5
Mariano Burguete <corsaiir@gmail.com>
2020-11-08 18:50:43 -0300

    Agregado de endpoints en init

--- backend/api/app/__init__.py
+++ backend/api/app/__init__.py
@@ -9,6 +9,8 @@ from app.resources.authResources import auth_bp
 from app.resources.jobsResources import jobs_bp
 from app.resources.interviewsResources import interviews_bp
 from app.resources.applicationsResources import applications_bp
+from app.resources.companiesResources import companies_bp
+from app.resources.RatingsResources import rating_bp
 from .ext import ma, migrate
 
 #MODELOS PARA REGISTRARLOS EN LA BASE
@@ -51,6 +53,8 @@ def create_app(settings_module):
     app.register_blueprint(jobs_bp)
     app.register_blueprint(interviews_bp)
     app.register_blueprint(applications_bp)
+    app.register_blueprint(companies_bp)
+    app.register_blueprint(rating_bp)
 
     # Registra manejadores de errores personalizados
     register_error_handlers(app)
